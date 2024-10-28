ede1b2a583d8cf49befc5d0be1b289abf4cc30cd
Pavanjangle <pjangale88@gmail.com>
2024-10-28 14:47:36 +0530

    add mantine custom theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "@mantine/core/styles.css";
+import customTheme from '../theme';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,7 +19,6 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
-
 const queryClient = new QueryClient();
 
 export default function RootLayout({
@@ -32,7 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* Wrap MantineProvider around MSWProvider */}
-        <MantineProvider>
+        <MantineProvider theme={customTheme}>
           <QueryClientProvider client={queryClient}>
             <MSWProvider>{children}</MSWProvider>
             <ReactQueryDevtools initialIsOpen={true} />
